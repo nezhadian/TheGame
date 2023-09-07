6174483f3111c864289a9b30d0809e962e86bcc4
nezhadian <yasin.ebr.nezh@gmail.com>
2023-09-07 18:38:35 +0430

    Register Data Added

---
+++ TheGame/Shared/RegisterData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheGame.Shared
+{
+    public class RegisterData
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required, MinLength(4, ErrorMessage = "Username must be at least 4 charcters")]
+        public string Username { get; set; }
+
+        [Required, MinLength(8, ErrorMessage = "Password must be at least 8 characters")]
+        public string Password { get; set; }
+
+        [Compare("Password",ErrorMessage ="Password is not match ")]
+        public string ConfirmPassword { get; set; }
+
+
+    }
+}
