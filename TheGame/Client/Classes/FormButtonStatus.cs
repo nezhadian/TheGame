8c56083507c9071762c0df5b0d2cf76296478749
nezhadian <yasin.ebr.nezh@gmail.com>
2023-09-08 10:24:50 +0430

    FormButtonStatus Added

---
+++ TheGame/Client/Classes/FormButtonStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheGame.Client.Classes
+{
+    public class FormButtonStatus
+    {
+        public enum ButtonStatus
+        {
+            Idle, Loading, Success, Error
+        }
+
+        public ButtonStatus Status { get; set; }
+        public string Message { get; set; }
+    }
+}
