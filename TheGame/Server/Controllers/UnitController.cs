6562607aadc946b6c34b966c9f6e5493984bcc4c
nezhadian <yasin.ebr.nezh@gmail.com>
2023-09-08 20:25:17 +0430

    Unit Api add and also getall route used for http get

---
+++ TheGame/Server/Controllers/UnitController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheGame.Server.Data;
+
+namespace TheGame.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class UnitController : ControllerBase
+    {
+        readonly DataContext _context;
+
+        public UnitController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("getall")]
+        public async Task<IActionResult> GetUnits()
+        {
+            var units = await _context.Units.ToListAsync();
+            return Ok(units);
+        }
+    }
+}
