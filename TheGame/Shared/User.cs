62a95271b253115e48321fa208b30c7fa71a7c65
nezhadian <yasin.ebr.nezh@gmail.com>
2023-09-08 06:49:16 +0430

    User Model Added

---
+++ TheGame/Shared/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheGame.Shared
+{
+    public class User
+    {
+        public int Id { get; set; }
+        
+        public string Email { get; set; }
+        public string Username { get; set; }
+        
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+
+        public int TotalCosts { get; set; } = 1000;
+        
+        public int Battles { get; set; }
+        public int Victories { get; set; }
+        public int Defeats { get; set; }
+    }
+}
