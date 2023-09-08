d364178ff856d8d24c735af5405b3f7cfe2fb65a
nezhadian <yasin.ebr.nezh@gmail.com>
2023-09-08 06:49:55 +0430

    Database Implemented

---
+++ TheGame/Server/Migrations/20230908021509_init.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TheGame.Server.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Email = table.Column<string>(type: "TEXT", nullable: true),
+                    Username = table.Column<string>(type: "TEXT", nullable: true),
+                    PasswordHash = table.Column<byte[]>(type: "BLOB", nullable: true),
+                    PasswordSalt = table.Column<byte[]>(type: "BLOB", nullable: true),
+                    TotalCosts = table.Column<int>(type: "INTEGER", nullable: false),
+                    Battles = table.Column<int>(type: "INTEGER", nullable: false),
+                    Victories = table.Column<int>(type: "INTEGER", nullable: false),
+                    Defeats = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- TheGame/Server/Startup.cs
+++ TheGame/Server/Startup.cs
@@ -2,10 +2,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Linq;
+using TheGame.Server.Data;
 
 namespace TheGame.Server
 {
@@ -22,6 +24,12 @@ namespace TheGame.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //me
+            services.AddScoped<IAuthRepository, AuthRepository>();
+
+            services.AddDbContext<DataContext>(options =>
+                   options.UseSqlite(Configuration.GetConnectionString("Default")));
+
 
             services.AddControllersWithViews();
             services.AddRazorPages();
