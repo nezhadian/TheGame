b53e90347b87cd0b885685d6cf44f41ad3ec5c6b
nezhadian <yasin.ebr.nezh@gmail.com>
2023-09-08 17:46:34 +0430

    User GetCosts Api Implemented

---
+++ TheGame/Server/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheGame.Server.Services;
+
+namespace TheGame.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class UserController : ControllerBase
+    {
+        private readonly IUtilityService _utility;
+
+        public UserController(IUtilityService utility)
+        {
+            _utility = utility;
+        }
+
+        [HttpGet("costs")]
+        public async Task<IActionResult> GetCosts()
+        {
+            var user = await _utility.GetUser();
+            return Ok(user.TotalCosts);
+        }
+    }
+}
