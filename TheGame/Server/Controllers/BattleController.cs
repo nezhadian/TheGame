c10c54ef6ce028585b45aff8c93055f941a77f18
nezhadian <yasin.ebr.nezh@gmail.com>
2023-09-09 20:12:38 +0430

    Implement Batttle Logic

--- TheGame/Server/Controllers/BattleController.cs
+++ TheGame/Server/Controllers/BattleController.cs
@@ -115,8 +115,125 @@ namespace TheGame.Server.Controllers
                 (u.AttackerId == userId || u.OpponentId == userId));
         }
 
+        
+        [HttpPost("attack")]
+        public async Task<IActionResult> AttackInBattle([FromBody] int battleId)
+        {
+            //find the battle
+            var battle = await _context.Battles.FindAsync(battleId);
+
+            if (battle.IsCompleted)
+                return BadRequest("this battle is end");
+
+            //declare vars
+            var attacks = new List<Attack>();
+
+            var attackerArmy = await GetArmy(battle.AttackerId);
+            var opponentArmy = await GetArmy(battle.OpponentId);
+            int round = battle.Rounds;
+
+            //fight
+            for (int i = 0; i < 50 && attackerArmy.Count > 0 && opponentArmy.Count > 0; i++)
+            {
+                round++;
+                if (round % 2 == 0)
+                {
+                    battle.OpponentDamage += FightRound(attackerArmy, opponentArmy, battle, round, attacks);
+                }
+                else
+                {
+                    battle.AttackerDamage += FightRound(opponentArmy, attackerArmy, battle, round, attacks);
+
+                }
+            }
+
+            //finish
+            if (attackerArmy.Count == 0 || opponentArmy.Count == 0)
+            {
+                FinishBattle(battle, opponentArmy.Count == 0, round);
+            }
+            
+
+            return Ok(attacks);
+        }
+
+        private static void FinishBattle(Battle battle, bool isAttackerWinner, int round)
+        {
+
+            if (isAttackerWinner)
+            {
+                battle.WinnerId = battle.AttackerId;
+                battle.Attacker.TotalCosts += battle.OpponentDamage;
+                battle.Opponent.TotalCosts += battle.AttackerDamage * 10;
+            }
+            else
+            {
+                battle.WinnerId = battle.OpponentId;
+                battle.Opponent.TotalCosts += battle.AttackerDamage;
+                battle.Attacker.TotalCosts += battle.OpponentDamage * 10;
+            }
+
+            battle.IsCompleted = true;
+            battle.Rounds = round;
+        }
+
+        private int FightRound(IList<UserUnit> attackerArmy, IList<UserUnit> opponentArmy, Battle battle, int round, List<Attack> attacks)
+        {
+            var rnd = new Random();
+
+            //select units
+            var randomAttacker = attackerArmy[rnd.Next(attackerArmy.Count)];
+            var randomOpponent = opponentArmy[rnd.Next(opponentArmy.Count)];
+
+            //damge
+            var damage = rnd.Next(randomAttacker.Unit.Attack) - rnd.Next(randomOpponent.Unit.Defence);
+
+            damage = damage < 0 ? 0 : damage;
+            //log
+
+            if(damage > randomOpponent.HitPoint)
+            {
+                //unit death
+                damage = randomOpponent.HitPoint;
+                randomOpponent.HitPoint -= damage;
+                opponentArmy.Remove(randomOpponent);
+
+                attacks.Add(new Attack {
+                    BattleId = battle.Id,
+                    AttackerUnitId = randomAttacker.Id,
+                    OpponentUnitId = randomOpponent.Id,
+                    Damage = damage,
+                    Round = round,
+                    IsUnitDead = true
+                });
+            }
+            else
+            {
+                //unit damaged
+                randomAttacker.HitPoint -= damage;
+                attacks.Add(new Attack
+                {
+                    BattleId = battle.Id,
+                    AttackerUnitId = randomAttacker.Id,
+                    OpponentUnitId = randomOpponent.Id,
+                    Damage = damage,
+                    Round = round
+                });
+            }
+
+            return damage;
 
 
+        }
 
+        private async Task<IList<UserUnit>> GetArmy(int userId)
+        {
+            var userUnits = await _context.UserUnits
+                .Where(u => u.UserId == userId && u.HitPoint > 0)
+                .Include(u => u.Unit)
+                .ToListAsync();
+
+            return userUnits;
+        }
     }
 }
