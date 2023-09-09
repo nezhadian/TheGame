946fc96e6adad4559c9c6df6af46a4909dc15baf
nezhadian <yasin.ebr.nezh@gmail.com>
2023-09-09 18:35:45 +0430

    Start Battle Api Implemented

---
+++ TheGame/Server/Controllers/BattleController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheGame.Server.Data;
+using TheGame.Server.Services;
+using TheGame.Shared;
+
+namespace TheGame.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class BattleController : ControllerBase
+    {
+        readonly IUtilityService _utility;
+        readonly DataContext _context;
+
+        public BattleController(IUtilityService utility, DataContext context)
+        {
+            _utility = utility;
+            _context = context;
+        }
+
+        [HttpPost("start")]
+        public async Task<IActionResult> StartBattle([FromBody] int opponentId)
+        {
+            var attacker = await _utility.GetUser();
+            var opponent = await _context.Users.FindAsync(opponentId);
+
+            if (await IsInBattle(attacker.Id))
+                return BadRequest($"Attacker ({attacker.Username}) is in battle");
+
+            if (await IsInBattle(opponent.Id))
+                return BadRequest($"Opponent ({attacker.Username}) is in battle");
+
+            var attackerHitpoint = await CalculateHitpoint(attacker.Id);
+            var opponentHitpoint = await CalculateHitpoint(opponent.Id);
+
+            if(attackerHitpoint == 0)
+                return BadRequest($"Attacker ({attacker.Username}) is not have army");
+
+            if (opponentHitpoint == 0)
+                return BadRequest($"Opponent ({opponent.Username}) is not have army");
+
+            var battle = new Battle
+            {
+                AttackerId = attacker.Id,
+                OpponentId = opponentId,
+                AttackerHitpoint = attackerHitpoint,
+                OpponentHitpoint = opponentHitpoint
+            };
+
+            _context.Battles.Add(battle);
+            await _context.SaveChangesAsync();
+
+            var battleProgress = new BattleProgress
+            {
+                BattleId = battle.Id,
+                AttackerName = attacker.Username,
+                OpponentName = opponent.Username,
+                AttackerHitpoint = attackerHitpoint,
+                OpponentHitpoint = opponentHitpoint
+            };
+
+            return Ok(battleProgress);
+
+        }
+
+        public async Task<int> CalculateHitpoint(int userId)
+        {
+            var count = await _context.UserUnits
+                .Select(u => u.UserId == userId ? u.HitPoint :0).SumAsync();
+
+            return count;
+        }
+        public async Task<bool> IsInBattle(int userId)
+        {
+            return await _context.Battles.AnyAsync(
+                u => !u.IsCompleted &&  
+                (u.AttackerId == userId || u.OpponentId == userId));
+        }
+
+
+
+
+    }
+}
---
+++ TheGame/Shared/BattleProgress.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheGame.Shared
+{
+    public class BattleProgress
+    {
+        public int BattleId { get; set; }
+        public string AttackerName { get; set; }
+        public string OpponentName { get; set; }
+        
+        public int AttackerDamage { get; set; }
+        public int AttackerHitpoint { get; set; }
+
+        public int OpponentDamage { get; set; }
+        public int OpponentHitpoint { get; set; }
+
+        public int Rounds { get; set; }
+
+
+    }
+}
