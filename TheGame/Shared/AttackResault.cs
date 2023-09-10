62df50baedfa9daa82b27077b2ac87528f445612
nezhadian <yasin.ebr.nezh@gmail.com>
2023-09-10 07:26:53 +0430

    Attack Resault Added

---
+++ TheGame/Shared/AttackResault.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheGame.Shared
+{
+    public class AttackResault
+    {
+        public int Round { get; set; }
+        public int AttackerUnitId { get; set; }
+        public int OpponentUnitId { get; set; }
+        public int Damage { get; set; }
+        public bool IsDead { get; set; }
+        public bool IsDefence { get; set; }
+
+
+    }
+}
