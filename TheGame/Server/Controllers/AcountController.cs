669d78b6ba0e9f2cd4f55bd112b80bc60a402548
nezhadian <yasin.ebr.nezh@gmail.com>
2023-09-18 14:38:29 +0430

    Implement ChangePassword Api in Acount Controller

---
+++ TheGame/Server/Controllers/AcountController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheGame.Server.Data;
+using TheGame.Server.Services;
+using TheGame.Shared;
+
+namespace TheGame.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class AcountController : ControllerBase
+    {
+        readonly DataContext _context;
+        private readonly IUtilityService _utility;
+
+        public AcountController(IUtilityService utility, DataContext context)
+        {
+            _utility = utility;
+            _context = context;
+        }
+
+        [HttpPost("changePassword")]
+        public async Task<IActionResult> ChangePassword(ChangePasswordData data)
+        {
+            var user = await _utility.GetUser();
+            if (!_utility.VerifyPasswordHash(user.PasswordHash, user.PasswordSalt, data.CurrentPassword))
+            {
+                return BadRequest(new ServiceResponse<string>
+                {
+                    IsSuccess = false,
+                    Message = "wrong password"
+                });
+            }
+            else
+            {
+                var password = _utility.GeneratePasswordHash(data.NewPassword);
+                user.PasswordHash = password.hash;
+                user.PasswordSalt = password.salt;
+                await _context.SaveChangesAsync();
+                return Ok(new ServiceResponse<string>
+                {
+                    IsSuccess = true,
+                    Data = "your password has been changed"
+                });
+            }
+        }
+
+    }
+}
---
+++ TheGame/Shared/ChangePasswordData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheGame.Shared
+{
+    public class ChangePasswordData
+    {
+        [Required, MinLength(8, ErrorMessage = "Password must be at least 8 characters")]
+        public string CurrentPassword { get; set; }
+
+        [Required, MinLength(8, ErrorMessage = "Password must be at least 8 characters")]
+        public string NewPassword { get; set; }
+
+        [Compare("NewPassword", ErrorMessage = "Password is not match ")]
+        public string ConfirmNewPassword { get; set; }
+    }
+}
